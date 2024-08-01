9202feac3138760eec0f1fc7a4c97a87e1169de9
shinhyeok22 <shinhyeok22@gmail.com>
2024-08-01 16:17:28 +0900

    Initial commit

---
+++ src/main/java/org/example/shoppjt/ShopPjtApplication.java
@@ -0,0 +1,13 @@
+package org.example.shoppjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopPjtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopPjtApplication.class, args);
+    }
+
+}
