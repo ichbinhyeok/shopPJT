882eb276d295fe7521d648b34e948dec3a8d26ba
shinhyeok22 <shinhyeok22@gmail.com>
2024-08-01 16:41:11 +0900

    BaseEntityTime 생성

---
+++ src/main/java/org/example/shoppjt/global/config/BaseEntityTime.java
@@ -0,0 +1,25 @@
+package org.example.shoppjt.global.config;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntityTime {
+
+    @CreatedDate // 데이터 생성날짜 자동 주입
+    @Column(updatable = false) // 업데이트 ㄴㄴ함 불변
+    private LocalDateTime createAt;
+
+    @LastModifiedDate// 데이터 수정날짜 자동주입
+    private LocalDateTime updateAt;
+}
